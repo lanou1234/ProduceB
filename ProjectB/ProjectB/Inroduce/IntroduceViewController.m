e56e5aa7c68178f3fda573b58edbd5990355e0fd
lanou <2465418814@qq.com>
2016-07-12 11:21:23 +0800

    1。新建introduce

---
+++ ProjectB/ProjectB/Inroduce/IntroduceViewController.m
@@ -0,0 +1,37 @@
+//
+//  IntroduceViewController.m
+//  ProjectB
+//
+//  Created by lanou on 16/7/12.
+//  Copyright © 2016年 H. All rights reserved.
+//
+
+#import "IntroduceViewController.h"
+
+@interface IntroduceViewController ()
+
+@end
+
+@implementation IntroduceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
